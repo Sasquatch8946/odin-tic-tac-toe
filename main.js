c501ccf83af75551f97096d26c801d4da323241c
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2025-05-03 11:02:33 -0500

    Make the various winning condition checks return the player's mark if true. Make isGameWon check that mark against the players' marks to determine who won.

--- main.js
+++ main.js
@@ -23,27 +23,35 @@ const game = (function () {
 
 function createPlayer (id) {
     let score = 0;
-    var mark;
+    let mark;
+    let name;
 
     if (id === 0) {
         mark = 'x';
+        name = 'Player 1';
     } else if (id === 1) {
         mark = 'o';
+        name = 'Player 2';
     }
     function playTurn(gameboard, row, column, mark) {
-        const playerMark = getPlayerMark();
+        const playerMark = getMark();
         gameboard[row][column] = playerMark;
         console.log(gameboard);
     }
 
-    function getPlayerMark() {
+    function getMark() {
         return mark;
+    }
+
+    function getName() {
+        return name;
     }
 
     return { 
         score,
         playTurn,
-        mark,
+        getMark,
+        getName,
     }
 }
 
@@ -82,19 +90,20 @@ function controlFlow(board) {
 const checks = (function (board) {
 
     function isRowEqual(board) {
-
+        let mark;
         let win = false;
         const isEqual = (row) => row.every(mark => mark === row[0] && mark !== null);
         board.forEach((row) => {
             if (isEqual(row)) {
                 win = true;
+                mark = row[0];
             }
         });
 
 
         if (win) {
             console.log("we have a winner! A row is equal")
-            return true;
+            return { mark };
         } else {
             console.log("no row is equal");
             return false;
@@ -102,10 +111,12 @@ const checks = (function (board) {
     }
     function isColumnEqual(board) {
         let win = false;
+        let mark;
         for (let col = 0; col < 3; col++) {
             const isEqual = board.every(row => row[col] === board[0][col] && row[col] !== null);
             if (isEqual) {
                 win = true;
+                mark = board[0][col];
             }
 
             
@@ -114,7 +125,7 @@ const checks = (function (board) {
 
         if (win) {
             console.log("we have a winner! A column is equal")
-            return true;
+            return { mark };
         } else {
             console.log("no column is equal");
             return false;
@@ -122,19 +133,29 @@ const checks = (function (board) {
     }
     function isDiagonalEqual(board) {
         function isDiagonal1Equal(board) {
-            return board[0][0] === board[1][1] && 
+            if (board[0][0] === board[1][1] && 
                 board[1][1] === board[2][2] && 
-                board[0][0] !== null;
+                board[0][0] !== null) {
+                    return board[0][0];
+            }
+            else {
+                return false;
+            }
         }
         function isDiagonal2Equal(board) {
-            return board[0][3] === board[1][1] && 
+        if (board[0][3] === board[1][1] && 
             board[1][1] === board[2][0] &&
-            board[0][3] !== null;
+            board[0][3] !== null) {
+                return board[0][3];
+            }
+            else {
+                return false;
+            }
         }
 
         if (isDiagonal1Equal(board) || isDiagonal2Equal(board)) {
             console.log("we have a winner! A diagonal is equal.");
-            return true;
+            return isDiagonal1Equal(board) ? isDiagonal1Equal(board) : isDiagonal2Equal(board);
         } else {
             console.log("no diagonal is equal");
             return false;
@@ -164,10 +185,20 @@ const checks = (function (board) {
 })();
 
 function isGameWon(board) {
-    if (checks.isRowEqual(board) ||
-        checks.isColumnEqual(board) ||
-        checks.isDiagonalEqual(board)) {
-            console.log("game is won!");
+    const isRowEqual = checks.isRowEqual(board);
+    const isColumnEqual = checks.isColumnEqual(board);
+    const isDiagonalEqual = checks.isDiagonalEqual(board);
+    const checkResults = [isRowEqual, isColumnEqual, isDiagonalEqual];
+    console.log(checkResults);
+    const gameWon = checkResults.filter((result) => result != false);
+    console.log(gameWon);
+    let winner;
+    if (gameWon.length === 1) {
+        console.log("game is won!");
+        console.log(`player's mark ${gameWon[0].mark}`);
+        winner = game.players.filter((p) => p.getMark() === gameWon[0].mark);
+        console.log(`${winner[0].getName()} won!`);
+
     } else if (checks.isBoardFull(board)) {
         console.log("game has not been won yet, but board is full. Game over.");
     } else {
